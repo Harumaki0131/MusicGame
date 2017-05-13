a6685c5314e3f2ed97848a2d5a40245e07b624d1
EriyaMurakami <harumakikazuto@gmail.com>
2017-05-13 20:30:18 +0900

    5/13らすと！

---
+++ Musicgame/PlayViewController.swift
@@ -0,0 +1,148 @@
+//
+//  PlayViewController.swift
+//  Musicgame
+//
+//  Created by yuki takei on 2017/02/25.
+//  Copyright © 2017年 EriyaMurakami. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import EZAudio
+import SpriteKit
+
+class PlayViewController: UIViewController{
+    
+    @IBOutlet var audioPlotFreq:EZAudioPlot!
+    
+    @IBOutlet var audioPlotTime:EZAudioPlot!
+    
+    @IBOutlet var soundBoardView:SKView!
+    
+    var soundBoardScene:SoundBoardScene = SoundBoardScene()
+    
+    var delegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスを取得
+    
+    var audio:AVAudioPlayer!
+    
+    var ezAudioFFT:EZAudioFFT!
+    
+    var audioFile:EZAudioFile!{
+        didSet{
+//            audioFile.delegate = self
+        }
+    }
+    
+    
+    var fft:EZAudioFFTRolling!
+    
+    let FFTViewControllerFFTWindowSize:vDSP_Length = 4096;
+    
+    var audioPlayer:EZAudioPlayer!
+//    var backAudioPlayer:EZAudioPlayer!
+
+    
+    override func viewDidLoad() {
+        openFileWithFilePathURL(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("syugabita", ofType: "mp3")!))
+        
+        audioPlayer = EZAudioPlayer()
+        audioPlayer.delegate = self
+        audioPlayer.playAudioFile(audioFile)
+        
+        ezAudioFFT = EZAudioFFT()
+        ezAudioFFT.delegate = self
+        
+        audioPlotTime.plotType = EZPlotType.Buffer;
+        
+        audioPlotFreq.shouldFill = true
+        audioPlotFreq.plotType = EZPlotType.Buffer
+        audioPlotFreq.shouldCenterYAxis = false
+        
+        fft = EZAudioFFTRolling(windowSize: FFTViewControllerFFTWindowSize, sampleRate: Float(self.audioPlayer.audioFile.clientFormat.mSampleRate), delegate: self)
+        
+        soundBoardView.backgroundColor = UIColor.clearColor()
+        
+        soundBoardScene.size = soundBoardView.frame.size
+        soundBoardView.presentScene(soundBoardScene)
+
+    }
+    
+    func openFileWithFilePathURL(filePathURL:NSURL){
+        self.audioFile = EZAudioFile(URL: filePathURL)
+//        self.audioFile.delegate = self
+        
+        var buffer = self.audioFile.getWaveformData().bufferForChannel(0)
+        var bufferSize = self.audioFile.getWaveformData().bufferSize
+        
+        //        print("openfile")
+        //        self.audioPlot.updateBuffer(buffer, withBufferSize: bufferSize)
+        
+        
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            
+            //            self.audioPlot.updateBuffer(buffer, withBufferSize: bufferSize)
+        })
+    }
+    
+    func ptrToArray(src: UnsafeMutablePointer<Float>, length: Int) -> Array<Float> {
+        var dst = [Float](count: length, repeatedValue: 0.0)
+        
+        dst.withUnsafeMutableBufferPointer({
+            ptr -> UnsafeMutablePointer<Float> in
+            return ptr.baseAddress
+        }).initializeFrom(src, count: length)
+        
+        return dst
+    }
+    
+    
+}
+
+//MARK: - EZAudioFFTDelegate
+extension PlayViewController: EZAudioFFTDelegate{
+    
+    
+    
+    func fft(fft: EZAudioFFT!, updatedWithFFTData fftData: UnsafeMutablePointer<Float>, bufferSize: vDSP_Length){
+        
+        //        print("fft")
+        var maxFrequency = fft.maxFrequency
+        var noteName = EZAudioUtilities.noteNameStringForFrequency(maxFrequency, includeOctave: true)
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            
+            //            print("noteName:\(noteName),maxFrequency:\(maxFrequency)")
+            self.audioPlotFreq.updateBuffer(fftData, withBufferSize: UInt32(40))
+            print(fftData[5]*100)
+            if fftData[5]*100 > 100 {
+                self.soundBoardScene.showNode()
+            }
+        })
+        
+        var fftArray = self.ptrToArray(fftData, length: Int(40))
+
+    }
+
+    
+}
+
+//MARK: - EZAudioPlayerDelegate
+extension PlayViewController: EZAudioPlayerDelegate{
+    
+    func audioPlayer(audioPlayer: EZAudioPlayer!, playedAudio buffer: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, withBufferSize bufferSize: UInt32, withNumberOfChannels numberOfChannels: UInt32, inAudioFile audioFile: EZAudioFile!){
+        
+        fft.computeFFTWithBuffer(buffer[0], withBufferSize: bufferSize)
+        
+        dispatch_async(dispatch_get_main_queue(),{
+            self.audioPlotTime.updateBuffer(buffer[0], withBufferSize: bufferSize)
+        })
+        
+    }
+}
+
+//extension PlayViewController: EZAudioFileDelegate{
+//    
+//}
+
+
---
+++ Musicgame/SoundBoardScene.swift
@@ -0,0 +1,103 @@
+//
+//  SoundBoardScene.swift
+//  Musicgame
+//
+//  Created by yuki takei on 2017/02/25.
+//  Copyright © 2017年 EriyaMurakami. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+
+class SoundBoardScene: SKScene, SKPhysicsContactDelegate {
+    
+    var nodeSize:CGFloat = 0.0
+    
+    var timer:NSTimer!
+    
+    var isNodeStop = false
+    
+    override func didMoveToView(view: SKView) {
+        super.didMoveToView(view)
+        
+        self.view?.backgroundColor = UIColor.clearColor()
+        self.physicsWorld.contactDelegate = self
+//        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: 1.0)
+        
+        
+        
+        
+        nodeSize = view.frame.width / 20
+        
+        for i in 0..<8{
+            
+        }
+        
+        var node:SKSpriteNode = SKSpriteNode()
+        node.color = UIColor.greenColor()
+        node.position = view.center
+        node.size = CGSize(width: nodeSize, height: nodeSize)
+        node.physicsBody = SKPhysicsBody(circleOfRadius: nodeSize/2)
+        node.physicsBody?.contactTestBitMask = 0x1 << UInt32(1)
+        node.physicsBody?.allowsRotation = false
+        node.physicsBody?.restitution = 0.0
+        node.physicsBody?.affectedByGravity = false
+        node.name = ""
+        addChild(node)
+        
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "timerUpDate:", userInfo: nil, repeats: true)
+
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        super.update(currentTime)
+
+    }
+    
+    func timerUpDate(timer:NSTimer){
+        
+        isNodeStop = false
+        
+    }
+    
+    func showNode(){
+        
+        if isNodeStop == false{
+            var node:SKSpriteNode = SKSpriteNode()
+            node.color = UIColor.greenColor()
+            node.position = self.view!.center
+            node.size = CGSize(width: nodeSize, height: nodeSize)
+            node.physicsBody = SKPhysicsBody(circleOfRadius: nodeSize/2)
+            node.physicsBody?.contactTestBitMask = 0x1 << UInt32(1)
+            node.physicsBody?.allowsRotation = false
+            node.physicsBody?.restitution = 0.0
+            node.physicsBody?.affectedByGravity = false
+            node.name = "test"
+            addChild(node)
+            
+//            let action = SKAction.move(to: , duration: 1.0)
+            
+            var houkou:UInt32 = 8
+            
+            let random = Int(arc4random_uniform(houkou))
+            var kakudo:Double!
+            
+            kakudo = Double(random) * 2 * M_PI / Double(houkou)
+            
+            
+            let action = SKAction.moveTo(CGPoint(x: self.view!.center.x + CGFloat(400 * cos(kakudo)), y: self.view!.center.y + CGFloat(400*sin(kakudo))), duration: 1.5)
+            node.runAction(action)
+            
+            
+            isNodeStop = true
+        }
+        
+        
+        
+    }
+
+}
+
